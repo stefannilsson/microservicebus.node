fc0314d514010d2a7c9f07ca1a2be12a8d41ee50
Mikael Håkansson <wmmihaa@hotmail.com>
2017-08-02 18:13:10 +0200

    dd

--- global.js
+++ global.js
@@ -2,7 +2,8 @@
 
 var homeDirectory = ".";
 var path = require("path");
-var module = require('module');
+var module = require('module').Module;
+
 // Check if node is started as Snap
 if (process.argv[1].endsWith("startsnap")) {
     console.log("Loading microservicebus.core/package.json for snap");
@@ -12,7 +13,14 @@ if (process.argv[1].endsWith("startsnap")) {
 var packagePath = path.resolve(homeDirectory, "node_modules");
 require('app-module-path').addPath(packagePath);
 module.globalPaths.push(packagePath);
+//require.main.paths.push(packagePath);
+
 module._initPaths();
+
+console.log('After');
+for (var i = 0; i < require.main.paths.length; i++) {
+    console.log(require.main.paths[i]);
+}
 
 console.log("microservicebus.node started as Snap");
 
