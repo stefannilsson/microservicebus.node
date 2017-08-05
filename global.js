dc2cf541c9b89b0284fa51fe9b8ab80accc0537d
Mikael Håkansson <wmmihaa@hotmail.com>
2017-08-05 20:29:45 +0200

    aaa

--- global.js
+++ global.js
@@ -1,12 +1,19 @@
 #!/usr/bin/env node
 
 
-var path = require("path");
-var packagePath = path.resolve(".", "node_modules");
+var packagePath = '/home/admin/node_modules';
 process.env.NODE_PATH = packagePath;
-
-console.log('packagePath: ' + packagePath);
-require('app-module-path').addPath(packagePath);
-require('module').globalPaths.push(packagePath);
+process.env.HOME = packagePath;
 require('module')._initPaths();
+
+
+//require('app-module-path').addPath('/home/admin/node_modules');
+
+//var path = require("path");
+//var packagePath = path.resolve(".", "node_modules");
+
+//console.log('packagePath: ' + packagePath);
+//require('app-module-path').addPath(packagePath);
+//require('module').globalPaths.push(packagePath);
+//require('module')._initPaths();
 require("./start.js");
