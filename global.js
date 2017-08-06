0f4dd5d152dfaad16e0f1e0ee895dcd59da9b799
Mikael Håkansson <wmmihaa@hotmail.com>
2017-08-06 21:39:14 +0200

    dd

--- global.js
+++ global.js
@@ -1,19 +1,50 @@
 #!/usr/bin/env node
 
+
 var path = require("path");
 var os = require('os');
 
 var packagePath = path.resolve(os.homedir(), 'node_modules');
+process.env.NODE_PATH = packagePath;
+process.env.HOME = os.homedir();
 
 if (process.argv.indexOf("--debug")) {
     process.env.NODE_DEBUG = "module";
 }
 
-//process.env.NODE_PATH = packagePath;
-//process.env.HOME = os.homedir();
+console.log('Before');
+for (var i = 0; i < require.main.paths.length; i++) {
+    console.log(require.main.paths[i]);
+}
+console.log('****************************');
+
+packagePath = path.resolve(".", "node_modules");
+console.log('packagePath: ' + packagePath);
 
 require('app-module-path').addPath(packagePath);
 require('module').globalPaths.push(packagePath);
+
+console.log();
+console.log('****************************');
+
+console.log('After (require.main.paths)');
+for (var i = 0; i < require.main.paths.length; i++) {
+    console.log(require.main.paths[i]);
+}
+console.log();
+console.log('After (globalPaths)');
+for (var i = 0; i < require('module').globalPaths.length; i++) {
+    console.log(require('module').globalPaths[i]);
+}
+
+console.log('****************************');
+console.log();
+
+//process.env.NODE_PATH = packagePath;
+//process.env.HOME = os.homedir();
+
+//require('app-module-path').addPath(packagePath);
+//require('module').globalPaths.push(packagePath);
 
 //require('module')._initPaths();
 //module.paths.unshift(packagePath);
--- start2.js
+++ start2.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 
-
-var m = require('module');
 var path = require("path");
-require('colors');
 
-
+console.log(process.argv);
+console.log("process.env.APPDATA: " + process.env.APPDATA);
+console.log("process.env.HOME: " + process.env.HOME);
+console.log("process.env.PWD: " + process.env.PWD);
+console.log("__dirname: " + __dirname);
+console.log(".: " + path.resolve("."));
+console.log("process.cwd: " + path.resolve(process.cwd()));
+console.log();
 console.log('****************************');
 
 console.log('Before');
@@ -19,6 +23,7 @@ console.log('packagePath: ' + packagePath);
 
 require('app-module-path').addPath(packagePath);
 require('module').Module.globalPaths.push(packagePath);
+console.log();
 console.log('****************************');
 
 console.log('After');
@@ -26,8 +31,9 @@ for (var i = 0; i < require.main.paths.length; i++) {
     console.log(require.main.paths[i]);
 }
 console.log('****************************');
-var util = require('./lib/Utils.js');
-var thermometer;
+console.log();
+
+require('colors');
 
 util.addNpmPackage("microservicebus.core@beta", true, function (err) {
     if (err) {
