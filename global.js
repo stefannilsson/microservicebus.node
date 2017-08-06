3b72de5d7f7e5b5e3dc0561464b2979dcfe444ea
Mikael Håkansson <wmmihaa@hotmail.com>
2017-08-06 19:08:13 +0200

    ...

--- global.js
+++ global.js
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
 
+var path = require("path");
+var packagePath = path.resolve(os.homedir(), 'node_modules');
 
-var packagePath = '/home/admin/node_modules';
 process.env.NODE_DEBUG = "module";
 process.env.NODE_PATH = packagePath;
-process.env.HOME = packagePath;
+process.env.HOME = os.homedir();
 
-require('module')._initPaths();
-module.paths.unshift(packagePath);
+require('app-module-path').addPath(packagePath);
+module.globalPaths.push(packagePath);
+
+//require('module')._initPaths();
+//module.paths.unshift(packagePath);
 
 require('colors');
 
@@ -20,4 +24,5 @@ require('colors');
 //require('app-module-path').addPath(packagePath);
 //require('module').globalPaths.push(packagePath);
 //require('module')._initPaths();
+
 require("./start.js");
